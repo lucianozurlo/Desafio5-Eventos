66a725911f8d4703117f838223c62a0e0539da07
lucianozurlo <49004308+lucianozurlo@users.noreply.github.com>
2022-02-28 18:44:18 -0300

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,5 +1,6 @@
 //Función que muestra resultado
 const lista = () => {
+
     calcularForm();
 
     //Declaro precios finales del asado
@@ -8,10 +9,11 @@ const lista = () => {
     precioEntranaFinal = asado[2].preciototal;
     precioCarbonFinal = asado[3].preciototal;
     totalPrecioAsado = asado.reduce((sumas, item) => sumas + item.preciototal, 0);
-    console.log(`Mostrar totales: ${totalPrecioAsado}`) // 66250
+    console.log(`Mostrar totales: ${totalPrecioAsado}`)
 
     //Creo un nuevo filtrando los array que no tienen un valor 0
     asadoOK = asado.filter((item) => item.peso !== 0);
+
 
     if (totalPrecioAsado !== 0) {
 
@@ -27,22 +29,22 @@ const lista = () => {
         //Defino la constante total con el resultado final
         total = parseFloat(totalPrecioAsado) - descuento;
 
-        let conclusion = document.querySelector("#conclusion");
-        conclusion.innerText = `Vas a necesitar llevar \$${total}`;
-        console.log(`Vas a necesitar llevar $${total}`)
-
-        let listado = document.querySelector("#listado");
+        listado = document.querySelector("#listado");
         listado.innerHTML = "<h3>Esto es lo que vas a necesitar comprar:</h3>";
 
         for (const corte of asadoOK) {
             listadoItems = document.createElement("li");
             listadoItems.innerHTML = `<strong> ${corte.nombre}:</strong> ${corte.peso}kg <em>(a $${corte.preciokilo} el kilo, son $${corte.preciototal})</em>`;
             listado.appendChild(listadoItems);
-
             console.log(`Listado final HTML: ${listadoItems.innerHTML}`)
         }
 
         listado.innerHTML += leyendaBeneficios;
+
+        let conclusion = document.querySelector("#conclusion");
+        conclusion.innerText = `Vas a necesitar llevar \$${total}`;
+        console.log(`Vas a necesitar llevar $${total}`)
+
     } else {
         conclusion = document.querySelector("#conclusion");
         conclusion.innerHTML = '¡No necesitás comprar nada!'; //Si no necesitás comprar nada no te muestra la opción de pagar con tarjeta de beneficios
@@ -51,8 +53,3 @@ const lista = () => {
 
     consola();
 }
-
-
-
-
-
